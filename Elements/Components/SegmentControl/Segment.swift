19b4462caa03963501cb56dfb410f77d63bd808f
feng.zhang-57blocks <feng.zhang@vungle.com>
2023-01-04 15:31:57 +0800

    feat: init project & add add segmentControl

---
+++ Elements/Components/SegmentControl/Segment.swift
@@ -0,0 +1,12 @@
+//
+//  Segment.swift
+//  Elements
+//
+//  Created by 57block on 2023/1/4.
+//
+
+import Foundation
+
+struct Segment {
+	
+}
---
+++ Elements/Components/SegmentControl/SegmentControl.swift
@@ -0,0 +1,8 @@
+//
+//  SegmentControl.swift
+//  Elements
+//
+//  Created by 57block on 2023/1/4.
+//
+
+import Foundation
